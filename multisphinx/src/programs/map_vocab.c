8e878c9afb15eae4f37d93c29bf73cd2edfc8ea9
dhdfu <dhdfu@94700074-3cef-4d97-a70e-9c8c206c02f5>
2010-10-20 03:19:44 +0000

    compiling but not working map vocab
    git-svn-id: svn://svn.code.sf.net/p/cmusphinx/code/trunk@10525 94700074-3cef-4d97-a70e-9c8c206c02f5

--- multisphinx/src/programs/map_vocab.c
+++ multisphinx/src/programs/map_vocab.c
@@ -50,9 +50,16 @@
 #include <sphinxbase/strfuncs.h>
 #include <sphinxbase/filename.h>
 #include <sphinxbase/byteorder.h>
+#include <sphinxbase/ngram_model.h>
+#include <sphinxbase/cmd_ln.h>
 
 /* PocketSphinx headers. */
 #include <pocketsphinx.h>
+
+/* S3kr3t headerz. */
+#include "pocketsphinx_internal.h"
+#include "fwdflat_search.h"
+#include "s2_semi_mgau.h"
 
 static const arg_t ps_args_def[] = {
     POCKETSPHINX_OPTIONS,
@@ -65,41 +72,110 @@ static const arg_t ps_args_def[] = {
       NULL,
       "Background dictionary file." },
     { "-prune_topn",
-      ARG_INT,
+      ARG_INTEGER,
       NULL,
       "In absence of -bgdict, prune vocabulary by taking top-N unigrams." },
     /* Other methods will certainly folllow */
     CMDLN_EMPTY_OPTION
 };
 
+/**
+ * Create synthetic senone scores using KL divergence of mixture weights.
+ */
 static void
 kl_score_senones(acmod_t *acmod, int senid)
 {
-	s2_semi_mgau_t *s;
-	int i, j, k;
+    s2_semi_mgau_t *s;
+    int i, j, k;
 
-	/* Of course this presumes semi continuous models. */
-	s = (s2_semi_mgau_t *)acmod->mgau;
-	/* Calculate KL divergence for all other senones. */
-	memset(acmod->senone_scores, 0,
-	       s->n_sen * sizeof(*acmod->senone_scores));
-	for (j = 0; j < s->n_feat; ++j) {
-		for (k = 0; k < s->n_density; ++k) {
-			float32 mixw = logmath_exp(s->lmath_8b,
-						   -s->mixw[j][k][senid]);
-			for (i = 0; i < s->n_sen; ++i) {
-				acmod->senone_scores[i]
-					+= (int)(mixw * (-s->mixw[j][k][senid]
-							 - -s->mixw[j][k][i]))
-					/* fake_kl_scaled */
-					/ s->n_feat;
-			}
-		}
-	}
+    /* Of course this presumes semi continuous models. */
+    s = (s2_semi_mgau_t *)acmod->mgau;
+    /* Calculate KL divergence for all other senones. */
+    memset(acmod->senone_scores, 0,
+           s->n_sen * sizeof(*acmod->senone_scores));
+    for (j = 0; j < s->n_feat; ++j) {
+        for (k = 0; k < s->n_density; ++k) {
+            float32 mixw = logmath_exp(s->lmath_8b,
+                                       -s->s->mixw[j][k][senid]);
+            for (i = 0; i < s->n_sen; ++i) {
+                acmod->senone_scores[i]
+                    += (int)(mixw * (-s->s->mixw[j][k][senid]
+                                     - -s->s->mixw[j][k][i]))
+                    /* fake_kl_scaled */
+                    / s->n_feat;
+            }
+        }
+    }
+}
+
+static dict_t *
+pruner_topn(dict_t *fulldict, ngram_model_t *fulllm, cmd_ln_t *config, acmod_t *acmod)
+{
+    
+    return NULL;
+}
+
+typedef struct {
+    char const *arg;
+    dict_t *(*func)(dict_t *fulldict, ngram_model_t *fulllm, cmd_ln_t *config, acmod_t *acmod);
+} pruner_t;
+
+static const pruner_t pruners[] = {
+    { "-prune_topn", &pruner_topn },
+    { NULL, NULL }
+};
+
+/**
+ * Generate a pruned dictionary according to the parameters in config.
+ */
+static dict_t *
+prune_dict(dict_t *fulldict, ngram_model_t *fulllm, cmd_ln_t *config, acmod_t *acmod)
+{
+    pruner_t const *p;
+
+    for (p = pruners; p->arg; ++p) {
+        if (cmd_ln_boolean_r(config, p->arg))
+            return (*p->func)(fulldict, fulllm, config, acmod);
+    }
+    return NULL;
 }
 
 int
 main(int argc, char *argv[])
 {
-	return 0;
+    ps_decoder_t *ps;
+    cmd_ln_t *config;
+    acmod_t *acmod;
+    ps_search_t *fwdflat;
+    dict_t *bgdict;
+    char const *str;
+
+    config = cmd_ln_parse_r(NULL, ps_args_def, argc, argv, TRUE);
+    /* Get the API to initialize a bunch of stuff for us (but not the search). */
+    /* FIXME: There will soon be a better way to do this. */
+    cmd_ln_set_boolean_r(config, "-fwdtree", FALSE);
+    cmd_ln_set_boolean_r(config, "-fwdflat", FALSE);
+    cmd_ln_set_boolean_r(config, "-bestpath", FALSE);
+    ps = ps_init(config);
+    acmod = ps->acmod;
+    fwdflat = fwdflat_search_init(config, acmod, ps->dict, ps->d2p, NULL);
+
+    /* Decide how to construct the background dictionary. */
+    if ((str = cmd_ln_str_r(config, "-bgdict"))) {
+        cmd_ln_t *c2 = cmd_ln_init(NULL, ps_args_def, TRUE,
+                                   "-dict", str, NULL);
+        bgdict = dict_init(c2, acmod->mdef);
+    }
+    else {
+        ngram_model_t *fulllm = ngram_model_read(config,
+                                                 cmd_ln_str_r(config, "-lm"),
+                                                 NGRAM_AUTO, ps->lmath);
+        bgdict = prune_dict(ps->dict, fulllm, config, acmod);
+        ngram_model_free(fulllm);
+    }
+    
+    ps_search_free(fwdflat);
+    ps_free(ps);
+
+    return 0;
 }
