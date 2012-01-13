b8977a0c57798706d8dcc471fedd5a4fd596d511
nshmyrev <nshmyrev@94700074-3cef-4d97-a70e-9c8c206c02f5>
2012-01-13 14:42:22 +0000

    Seprate ci tied states from cd tied states and sort them properly. Thanks to Pedro Batista for the test example.
    Now the problem remains that pocketsphinx fwdflat is still broken
    
    
    
    git-svn-id: svn://svn.code.sf.net/p/cmusphinx/code/trunk@11316 94700074-3cef-4d97-a70e-9c8c206c02f5

--- htk2s3conv/htk_converter.py
+++ htk2s3conv/htk_converter.py
@@ -10,7 +10,6 @@ from struct import unpack, pack
 import sys
 from sys import exit
 import time
-import operator
 
 from ply import *
 
@@ -69,6 +68,7 @@ class HtkConverter(object):
 	
 	# Dictionaries containing mappings from instances of State/Tmat to id's.
 	# This is needed to write the Sphinx 3 model files.
+	ciStatesToIds = {}
 	statesToIds = {}
 	tmatsToIds = {}
 		
@@ -149,7 +149,7 @@ class HtkConverter(object):
 		if len(badHmms) > 0:
 			names = " ".join([hmm.name for hmm in badHmms])
 			raise HtkConverterError('Transition matrix %s have different number of states than others. Such model can not be converted.' % (names))
-		
+
 		pr('HTK model files loaded and parsed.')
 	
 	# Display the loaded HTK models.
@@ -160,7 +160,32 @@ class HtkConverter(object):
 		
 		for hmm in hmms:
 			hmm.display()
+	
+	def _countItems(self, hmms):
+		# Create a set of all states.
+		states = unique([state for hmm in hmms for (iState, state) in hmm.states])
 		
+		# Create a set of all transition matrices.
+		tmats = unique([hmm.tmat for hmm in hmms])
+
+		# Monophone states
+		monophoneStates = unique([state for hmm in self.monophoneHmms for (iState, state) in hmm.states])
+		
+		# Create the State/Tmat-instance to id mappings, first we count all monophones
+		for state in monophoneStates:
+			self.ciStatesToIds[state] = len(self.ciStatesToIds)
+	
+		# Then count all states again for triphones
+		for state in states:
+			self.statesToIds[state] = len(self.statesToIds) + len(self.ciStatesToIds)
+		
+		for tmat in tmats:
+			self.tmatsToIds[tmat] = len(self.tmatsToIds)
+		
+		self.states = sorted(self.ciStatesToIds, key=self.ciStatesToIds.__getitem__)
+		self.states.extend(sorted(self.statesToIds, key=self.statesToIds.__getitem__))
+		self.tmats = sorted(self.tmatsToIds, key=self.tmatsToIds.__getitem__)
+	
 	# Create Sphinx 3 model files.
 	def writeS3(self, outprefix = ''):
 		hmms = self.hmms
@@ -168,25 +193,8 @@ class HtkConverter(object):
 			raise HtkConverterError('Please load the HTK models first.')
 		
 		self.outprefix = outprefix
-		
-		# Create a set of all states.
-		states = unique([state for hmm in hmms for (iState, state) in hmm.states])
-		self.states = states
-		
-		# Create a set of all transition matrices.
-		tmats = unique([hmm.tmat for hmm in hmms])
-		self.tmats = tmats
 
-		monophoneStates = unique([state for hmm in self.monophoneHmms for (iState, state) in hmm.states])
-		
-		# Create the State/Tmat-instance to id mappings.
-		for state in monophoneStates:
-			self.statesToIds[state] = len(self.statesToIds)
-		for state in states:
-			if not self.statesToIds.has_key(state):
-				self.statesToIds[state] = len(self.statesToIds)
-		for tmat in tmats:
-			self.tmatsToIds[tmat] = len(self.tmatsToIds)
+		self._countItems(hmms)
 		
 		self._writeS3Mdef(hmms)
 		self._writeS3MeanVar(hmms)
@@ -227,8 +235,7 @@ class HtkConverter(object):
 
 		# n_tied_ci_state: no. of (emitting) states for your 'base' phones
 		# after state-sharing is done.
-		monophoneStates = unique([state for hmm in self.monophoneHmms for (iState, state) in hmm.states])
-		file.write('%s n_tied_ci_state\n' % len(monophoneStates))
+		file.write('%s n_tied_ci_state\n' % len(self.ciStatesToIds))
 
 		# n_tied_tmat: The HMM for each CI phone has a transition
 		# probability matrix associated it. This is the total number of
@@ -279,21 +286,22 @@ class HtkConverter(object):
 		file.write('#\n')
 		file.write('# base lft rt p attrib tmat ... state id\'s ...\n')
 		for (tupleFrom, nameTo) in tiedlist:
+
+			hmm = self.namesToHmms[nameTo]
+
 			if tupleFrom[0] == 1:
 				blrp = tupleFrom[1:4] + ('-',)
+				stateIds = tuple([self.ciStatesToIds[state] for (iState, state) in hmm.states])
 			elif tupleFrom[0] == 3:
 				blrp = tupleFrom[1:4] + ('i',)
+				stateIds = tuple([self.statesToIds[state] for (iState, state) in hmm.states])
 			else:
 				raise HtkConverterError('Programming error: tupleFrom[0] == %s' % tupleFrom[0])
 			
 			if tupleFrom[1].lower() != 'sil':
 				a = 'n/a'
 			else:
-				a = 'filler'
-			
-			hmm = self.namesToHmms[nameTo]
-			
-			stateIds = tuple([self.statesToIds[state] for (iState, state) in hmm.states])
+				a = 'filler'			
 			
 			#         b  l  r  p  a  t       s
 			format = '%s\t%s\t%s\t%s\t%s\t%s\t' + '%s\t' * len(stateIds) + 'N\n'
